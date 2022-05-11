357e55c790ec8ecd33cb25888055546cf6fcd5e7
mkaufman <mkaufman@leantechniques.com>
2022-05-11 10:11:16 -0500

    refactor wip

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -38,39 +38,61 @@ namespace Tennis
             }
             else if (IsWinningScore())
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = SetAdvantageOrWinScore();
             }
             else
             {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else
-                    {
-                        score += "-";
-                        tempScore = m_score2;
-                    }
+                score = SetASDF(score);
+            }
 
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += Scores.Love;
-                            break;
-                        case 1:
-                            score += Scores.Fifteen;
-                            break;
-                        case 2:
-                            score += Scores.Thirty;
-                            break;
-                        case 3:
-                            score += Scores.Forty;
-                            break;
-                    }
+            return score;
+        }
+
+        private string SetASDF(string score)
+        {
+            int tempScore;
+            for (var i = 1; i < 3; i++)
+            {
+                if (i == 1) tempScore = m_score1;
+                else
+                {
+                    score += "-";
+                    tempScore = m_score2;
                 }
+
+                score = SetScore(tempScore, score);
+            }
+
+            return score;
+        }
+
+        private string SetAdvantageOrWinScore()
+        {
+            string score;
+            var minusResult = m_score1 - m_score2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+            return score;
+        }
+
+        private static string SetScore(int tempScore, string score)
+        {
+            switch (tempScore)
+            {
+                case 0:
+                    score += Scores.Love;
+                    break;
+                case 1:
+                    score += Scores.Fifteen;
+                    break;
+                case 2:
+                    score += Scores.Thirty;
+                    break;
+                case 3:
+                    score += Scores.Forty;
+                    break;
             }
 
             return score;
